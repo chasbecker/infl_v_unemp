6eaf7f9c5e01c7042393cd6f53b99e0b58b2a5a5
Charles Becker <c.l.becker@outlook.com>
2022-04-15 16:24:49 -0700

    major revision

--- main.R
+++ main.R
@@ -10,39 +10,40 @@ library(lubridate)
 
 # start with the raw file downloaded in Excel from FRED
 # their might be some pointless and inefficient flailing here
-# but I'm a "belt and suspenders" type of guy
+# but I'm a "belt and suspenders" type of guy.  And this separates
+# the transforms from the analysis
 
-u_tbl <- read.table( "./data/UNRATE.csv", sep=",", header=FALSE, skip=0 )
+u_tbl <- read.csv( "./data/UNRATE.csv", sep=",", header=FALSE, skip=0 )
 toskip <- which( u_tbl$V1 == "date" ) - 1
-u_tbl <- read.table( "./data/UNRATE.csv", sep=",", header=TRUE, skip=toskip )
-u_tbl$date <- mdy( u_tbl$date )
-write.csv( u_tbl, "./data/uempl.csv", sep=",", row.names=FALSE )
+u_tbl <- read.csv( "./data/UNRATE.csv", sep=",", header=TRUE, skip=toskip )
+u_tbl$YMDate <- mdy( u_tbl$date )
+u_tbl$Unempl <- u_tbl$value
+u_tbl <- subset( u_tbl, select = -c(date, value))
+write.csv( u_tbl, "./data/uempl.csv", row.names=FALSE )
 
-i_tbl <- read.table( "./data/CPILFESL.csv", sep=",", header=FALSE, skip=0 )
+#
+# this is 'core' inflation
+#
+i_tbl <- read.csv( "./data/CPILFESL.csv", sep=",", header=FALSE, skip=0 )
 toskip <- which( i_tbl$V1 == "date" ) - 1
-i_tbl <- read.table( "./data/CPILFESL.csv", sep=",", header=TRUE, skip=toskip )
-i_tbl$date <- mdy( i_tbl$date )
+i_tbl <- read.csv( "./data/CPILFESL.csv", sep=",", header=TRUE, skip=toskip )
+i_tbl$YMDate <- mdy( i_tbl$date )
+i_tbl$CPI <- i_tbl$value
+i_tbl <- subset( i_tbl, select = -c(date, value))
+# calculate month-to-month inflation from previous and current CPI
+i_tbl <- (mutate( i_tbl, Inflx1000 = ( (CPI - lag(CPI) ) / lag(CPI) )*1000 ))
+i_tbl$Inflx1000 <- round( i_tbl$Inflx1000, digits = 1 )
 write.csv( i_tbl, "./data/infl.csv", sep=",", row.names=FALSE)
 
-
-
-
-
-
-i <- read.csv( "./data/infl.csv" )
-# here need to calculate "inflation", then inflation * 10"
-i["Infl*10"] <- round( i["Infl*10"], 1 )
-u <- read_csv( "./data/uempl.csv")
-
 #### on pause
-iu <- inner_join( i, u, by="date" )
-iu$date <- mdy( iu$date ) 
-iu <- iu %>% rename(  CPI = value.x, Unempl = value.y, Inflx10 = "Infl*10"  )
+iu_tbl <- inner_join( i_tbl, u_tbl , by="YMDate" )
+iu_tbl <- subset( iu_tbl, select = -c(CPI) )
 
-explPlot1 <- ggplot( data = iu )+
-                geom_line( aes(y=Inflx10, x= date, colour="Infl"),size=1 )+
-                geom_line( aes(y=Unempl, x= date, colour="Unempl"),size=1) +
-                scale_color_manual(name = "Infl_v_Unempl", values = c("Infl" = "chartreuse", "Unempl" = "deeppink1"))
+
+explPlot1 <- ggplot( data = iu_tbl )+
+                geom_line( aes(y=Inflx1000, x= YMDate, colour="Inflx1000"),size=1 )+
+                geom_line( aes(y=Unempl, x= YMDate, colour="Unempl"),size=1) +
+                scale_color_manual(name = "Infl_v_Unempl", values = c("Inflx1000" = "chartreuse", "Unempl" = "deeppink1"))
   
 explPlot1
 
