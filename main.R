0e32cefd58717784cc5fc0343b7ae18c99407318
Charles Becker <c.l.becker@outlook.com>
2022-04-18 15:33:56 -0700

    more cleanup again

--- main.R
+++ main.R
@@ -3,28 +3,28 @@
 #
 # all FRED data from the St Louis FED
 
-#  Need these two packages for this project
+#  Needed for this project
 #install.packages("fredr")
 #install.packages("usethis")
 
 rm(list=ls())
 library(fredr)
-# library(usethis)  # to edit .Renviron file <***>
+# library(usethis)  # to edit .Renviron file one time <***>>>
 library(tidyverse)
 
 # get FRED API key, save to environment variable
 
 # 1) registered for FRED account
 # 2) open a .Renviron file (uncomment line below)
-# edit_r_environ() <***>
-# 3) add API key like so
+# edit_r_environ() <<<***>
+# 3) add API key to .Renviron file like so
 # 3.1) FRED_API_KEY=abcdefghijklmnopqrstuvwxyz123456
 # 4) save and close the .Renviron file
 # 5) restart R & RStudio
 
 # read, transform, save unemployment data from FRED
 u_tbl <- fredr(series_id = "UNRATE")
-u_tbl$unempl <- u_tbl$value
+u_tbl <- rename( u_tbl, unempl = value )
 u_tbl <- subset( u_tbl, select = c(date, unempl))
 write.csv( u_tbl, "./data/unempl.csv", row.names=FALSE )
 
@@ -33,8 +33,7 @@ i_tbl <- fredr(series_id = "CPIAUCSL")
 # rename the 'value' column to 'cpi' to make the 'mutate' statement more readable
 i_tbl <- rename(i_tbl, CPI = value )
 # calculate monthly inflation,
-# multiply by 10 to put in similar range to unemployment
-# and round to one decimal
+# multiply by 10 to normalize to unemployment, and round to one decimal
 i_tbl <- (mutate( i_tbl, inflx10 = ( (CPI - lag(CPI) ) / lag(CPI) )*1000 ))
 i_tbl$inflx10 <- round( i_tbl$inflx10, digits = 1 )
 i_tbl <- subset( i_tbl, select = c(date, inflx10))
